a78465275c650a89e1bb400ef273b52b613f804d
yzz <4506142@qq.com>
2018-03-29 10:03:58 +0800

    项目初始化

--- src/main/java/com/ztjs/smartSite/controller/UserController.java
+++ src/main/java/com/ztjs/smartSite/controller/UserController.java
@@ -6,7 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.ztjs.smartSite.pojo.User;
 import com.ztjs.smartSite.service.IUserService;
 
@@ -17,10 +21,17 @@ public class UserController {
 	private IUserService userService;
 	
 	@RequestMapping("/showUser")
-	public String toIndex(HttpServletRequest request,Model model){
+	@ResponseBody
+	public JSONArray toIndex(HttpServletRequest request){
 		int userId = Integer.parseInt(request.getParameter("id"));
 		User user = this.userService.getUserById(userId);
-		model.addAttribute("user", user);
-		return "showUser";
+		JSONObject a=new JSONObject();
+		JSONObject c=new JSONObject();
+		JSONArray b=new JSONArray();
+		a.put("1", 2);
+		c.put("2", 1);
+		b.add(a);
+		b.add(c);
+		return b;
 	}
 }
